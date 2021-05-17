8e28154d0bc26c737a606fe37bd5fd1101e0031a
rostik307 <307rostik@gmail.com>
2021-05-17 13:10:05 +0200

    Cambio de menú

--- menu.php
+++ menu.php
@@ -16,28 +16,25 @@ if(!isset($_SESSION["login"])){
 	<link rel="stylesheet" href="css/estilo.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <body>
-   
-      <nav class="w3-sidebar w3-bar-block w3-card w3-animate-left w3-center" style="display:none" id="mySidebar">
-        <a href="menu.php" class="w3-bar-item w3-button">Menu</a>
-		<a href="paginas/login.html" class="w3-bar-item w3-button">Login</a>
-        <a href="paginas/prestamos.php" class="w3-bar-item w3-button">Prestamos</a>
-        <a href="paginas/historial.php" class="w3-bar-item w3-button">Historial</a>
-        <a href="paginas/mostrar-datos.php" class="w3-bar-item w3-button">Inventario</a> 
-		<a href="paginas/logout.php" class="w3-bar-item w3-buttom">Cerrar Sesion</a>
-		<button class="w3-bar-item w3-button" onclick="w3_close()">Close <i class="fa fa-remove"></i></button>
-      </nav>
+
+		<nav class="w3-sidebar w3-bar-block w3-card w3-animate-left w3-center" style="display:none" id="mySidebar">
+			<a href="menu.html" class="w3-bar-item w3-button">Menu</a>
+			<a href="paginas/login.html" class="w3-bar-item w3-button">Login</a>
+			<a href="paginas/prestamos.php" class="w3-bar-item w3-button">Prestamos</a>
+			<a href="paginas/historial.php" class="w3-bar-item w3-button">Historial</a>
+			<a href="paginas/mostrar-datos.php" class="w3-bar-item w3-button">Inventario</a> 
+			<button class="w3-bar-item w3-button" onclick="w3_close()">Close <i class="fa fa-remove"></i></button>
+		</nav>
 	  
-    <header class="cabecera" id="myHeader">
-        <i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-theme"></i> 
-		<h1 class="fa fa-user w3-display-topright w3-xlarge"> Usuario: <?php echo "$login"; ?></h1>
-		<div class="w3-center">
-			<img src="img/logo.png" alt="" width="100" height="100"/>
-			<h1 class="w3-xxxlarge w3-animate-bottom"><b>IES ALBERICIA</b></h1>
-			<h1 class="w3-xxxlarge w3-animate-bottom"><b>ALMACENES</b></h1>
-			<div class="w3-padding-64"/>
-		</div>
-    </header>
-	
+		<header class="cabecera" id="myHeader">
+			<i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-theme"></i> 
+			<div class="w3-center">
+				<img src="img/logo.png" alt="" width="100" height="100"/>
+				<h1 class="w3-xxxlarge w3-animate-bottom"><b>IES ALBERICIA</b></h1>
+				<h1 class="w3-xxxlarge w3-animate-bottom"><b>ALMACENES</b></h1>
+				<div class="w3-padding-64"/>
+			</div>
+		</header>
 	
 	<div id="id01" class="w3-modal">
 		<div class="w3-modal-content w3-card-4 w3-animate-top">
@@ -53,62 +50,71 @@ if(!isset($_SESSION["login"])){
 		</div>
 	</div>
 	  
-    <div class="w3-row-padding w3-center w3-margin-top">
+    <div id="id01" class="w3-modal">
+			<div class="w3-modal-content w3-card-4 w3-animate-top">
+			
+				<header class="w3-container w3-theme-l1">
+					<span onclick="document.getElementById('id01').style.display='none'"
+						class="w3-button w3-display-topright">×</span>
+					<h4>Conectarse</h4>
+				</header>	
+				
+			</div>
+		</div>
+	  
+		<div class="w3-row-padding w3-center w3-margin-top">
 	
-		<a href="paginas/prestamos.html">
-			<div class="w3-third">
-				<div class="w3-card w3-container" style="min-height:460px">
-					<h3><b>Préstamos</b></h3>
-					<br>
-					<i class="fa fa-users w3-margin-bottom w3-text-theme" style="font-size:200px"></i>
-					<b>
-						<p>
-						Pincha aqui para poner un aviso de que
-						has tomado algo o para solicitar un préstamo
-						</p>
-					</b>
-				</div>
-			</div>
-		</a>
-		
-		<a>
-			<div class="w3-third">
-				<div class="w3-card w3-container" style="min-height:460px">
-				    <h3><b>Historial</b></h3>
-				    <br>
-				    <i class="fa fa-book w3-margin-bottom w3-text-theme" style="font-size:200px"></i>
-				    <b>
-						<p>
-						Ver los préstamos realizados en el último
-						</p>
-					</b>
-					<button type="button" class="btn-registros">Día</button>
-					<button type="button" class="btn-registros">Semana</button>
-					<button type="button" class="btn-registros">Mes</button>
-				</div>
-			</div>
-		</a>
-		
-		<a href="paginas/mostrar-datos.php">
-			 <div class="w3-third">
-				<div class="w3-card w3-container" style="min-height:460px">
-					<h3><b>Inventario</b></h3>
-					<br>
-					<i class="fa fa-cubes w3-margin-bottom w3-text-theme" style="font-size:200px"></i>
-
-					<div class="container">
+			<a href="paginas/prestamos.php">
+				<div class="w3-third">
+					<div class="w3-card w3-container" style="min-height:460px">
+						<h3><b>Préstamos</b></h3>
+						<br>
+						<i class="fa fa-users w3-margin-bottom w3-text-theme" style="font-size:200px"></i>
 						<b>
 							<p>
-							Revisa todos los objetos de nuestros almacenes
-							y en cual de ellos se encuentran situados
+							Pincha aqui para poner un aviso de que
+							has tomado algo o para solicitar un préstamo
 							</p>
 						</b>
 					</div>
 				</div>
-			 </div>
-		 </a>
+			</a>
+			
+			<a href="paginas/historial.php">
+				<div class="w3-third">
+					<div class="w3-card w3-container" style="min-height:460px">
+						<h3><b>Historial</b></h3>
+						<br>
+						<i class="fa fa-book w3-margin-bottom w3-text-theme" style="font-size:200px"></i>
+						<b>
+							<p>
+							Ver los préstamos realizados
+							</p>
+						</b>
+					</div>
+				</div>
+			</a>
+			
+			<a href="paginas/mostrar-datos.php">
+				 <div class="w3-third">
+					<div class="w3-card w3-container" style="min-height:460px">
+						<h3><b>Inventario</b></h3>
+						<br>
+						<i class="fa fa-cubes w3-margin-bottom w3-text-theme" style="font-size:200px"></i>
+
+						<div class="container">
+							<b>
+								<p>
+								Revisa todos los objetos de nuestros almacenes
+								y en cual de ellos se encuentran situados
+								</p>
+							</b>
+						</div>
+					</div>
+				 </div>
+			 </a>
 		
-    </div>
+		</div>
 	  
 
 		<script>
