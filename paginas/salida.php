346921cd298601dffc49b5a7a226e434ee33d5c4
Magenstor <82580166+Magenstor@users.noreply.github.com>
2021-05-19 12:54:12 +0200

    Update salida.php

--- paginas/salida.php
+++ paginas/salida.php
@@ -1,4 +1,15 @@
 <html>
+	<head>
+		<title>ALMACENES ALBERICIA</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="../css/temas.css">
+		<link rel="stylesheet" href="../css/bootstrap.css">
+		<link rel="stylesheet" href="../css/w3.css">
+		<link rel="stylesheet" href="../css/estilo.css"> 
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
 <div class="portatablas ">
 
 	
@@ -33,4 +44,5 @@
            <?php 
        }
     ?>
+	</body>
 	</html>
